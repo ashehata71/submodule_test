ff13ba4198c38dd8021c8658796ca8519b5b8f81
shehata <ahmed.fcis2016@gmail.com>
2025-05-22 00:43:51 +0300

    - Change the message

--- lib/greeting_user.dart
+++ lib/greeting_user.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class GreetingUser {
   Widget greetingUser() => const Text(
-        "Welcome back!",
+        "Welcome back our dear!",
         style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.w500,
