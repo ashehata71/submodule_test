559f616a88932db2d4ebbbc73fb16f53895ad6ca
shehata <ahmed.fcis2016@gmail.com>
2025-05-25 10:36:34 +0300

    --

--- lib/greeting_user.dart
+++ lib/greeting_user.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-Widget get greetingUser => const Text(
-      "Welcome back our dear!",
-      style: TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.w700,
-        color: Colors.red, // Set the message color
-      ),
-    );
+class GreetingUser {
+  static Widget get greetingUser => Text(
+        "Welcome back our dear!",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          color: Colors.red, // Set the message color
+        ),
+      );
+}
