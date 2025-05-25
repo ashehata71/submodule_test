1d53629c4668d431095341120906d894251adac4
shehata <ahmed.fcis2016@gmail.com>
2025-05-25 10:40:36 +0300

    --

--- lib/greeting_user.dart
+++ lib/greeting_user.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class GreetingUser {
-  static Widget get greetingUser => Text(
+  static Widget get greetingUser => const Text(
         "Welcome back our dear!",
         style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.w700,
-          color: Colors.red, // Set the message color
+          color: Colors.redAccent, // Set the message color
         ),
       );
 }
