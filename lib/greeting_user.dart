c29750e7a1908bd6b3d010d3a55d6953ca42f9de
shehata <ahmed.fcis2016@gmail.com>
2025-05-25 12:18:29 +0300

    --

--- lib/greeting_user.dart
+++ lib/greeting_user.dart
@@ -5,7 +5,7 @@ class GreetingUser {
         "Welcome back our dear!",
         style: TextStyle(
           fontSize: 20,
-          fontWeight: FontWeight.w300,
+          fontWeight: FontWeight.w400,
           color: Colors.grey, // Set the message color
         ),
       );
