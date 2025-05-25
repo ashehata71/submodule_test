b91d570bfad33f15c58ef3fc81c82700bbc1eb78
shehata <ahmed.fcis2016@gmail.com>
2025-05-25 10:22:10 +0300

    - Added comments

--- lib/greeting_user.dart
+++ lib/greeting_user.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 class GreetingUser {
   Widget greetingUser() => const Text(
-        "Welcome back our dear!",
+        "Welcome back our dear!", // This is the greeting message
         style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.w700,
-          color: Colors.grey,
+          color: Colors.grey, // Set the message color
         ),
       );
 }
